5587db963ddfc1afebb038a953cfd32d46744990
Oleksandr Knyga <oleksandrknyga@gmail.com>
2022-04-10 01:44:24 +0300

    init

---
+++ README.md
@@ -0,0 +1,94 @@
+# DDOS Load Testing Server
+HTTP/TCP/UDP server to generate benchmark for benchmark tools.
+
+## Where to find benchmark tools
+* https://github.com/denji/awesome-http-benchmark
+* https://github.com/MatrixTM/MHDDoS
+* https://github.com/alexmon1989/russia_ddos
+
+## API
+
+### Parts
+1. HTTP Control API. It is started with process boot.
+2. Benchmark Server. It is started with start request for HTTP Control API.
+
+### HTTP Control API is REST API with methods
+1. `HTTP GET /start/:type/:port/:duration?`. 
+1.1. Request resets Stats and boots up Server of Type on the Port. It changes Status to `Started`.
+1.2. If Status is not `Stopped` then it will return 400 HTTP code. Only one Server could run at the same time.
+1.3. If case of success then it will return 200 HTTP code.
+1.4. If Server can't be started then it will return 403 HTTP code.
+1.5. Duration is an optional param. It specifies lifetime of the Benchmark Server in seconds.
+2. `HTTP GET /stop`.
+2.1. Request stops Stats accumulation, kills Server, and returns Stats. It changes Status to `Stopped`.
+2.2. If Status is not `Started` then it will return 400 HTTP code.
+2.3. If case of success it will return 200 HTTP code and Stats object.
+3. `HTTP GET /stats`.
+3.1. Request returns latests Stats accumulation without changing State.
+3.2. If Status is not `Started` then it will return 400 HTTP code.
+3.3. If case of success it will return 200 HTTP code and Stats object.
+
+### Object details
+```typescript
+enum Status {
+  Started,
+  Stopped,
+}
+
+enum Type {
+  TCP = 'tcp',
+  UDP = 'udp',
+  HTTP = 'http',
+}
+
+interface Stats {
+  type: Type,
+  duration: {
+    seconds: Number,
+  },
+  requests: {
+    total: {
+      count: Number,
+      bytes: Number,
+    },
+    average: {
+      perSeconds: {
+        count: Number,
+        bytes: Number,
+      },
+    },
+  },
+}
+```
+
+## How to start
+
+### Without Docker
+
+#### Prerequisites
+1. Node.js >= 14.17.6.
+2. NPM >= 6.14.15.
+
+#### Installation
+1. `npm i`.
+
+#### Start
+1. To start HTTP Control API on port 8070: `npm run start -- -p 8070` or `node app -p 8070`.
+
+#### Remarks
+1. Default HTTP Control API port in 8080.
+2. Access to some ports might require root permissions.
+3. You can't start multiple servers of same type.
+
+### With Docker
+Docker is not implemented yet.
+
+#### Prerequisites
+1. Docker.
+
+#### Start
+1. To start HTTP Control API on port 8070 and preplanned exposed port (UDP) for Benchmark Server 8053: `docker run -it --rm oknyga/ddos-load-testing-server:latest -p 8070:8080/tcp -p 8053:8053/udp`.
+
+#### Remarks
+1. Same remarks as for Without Docker.
+2. Plan ports you will be using ahead, read Published ports in Container networking documentation of the Docker: https://docs.docker.com/config/containers/container-networking/
---
+++ app.js
@@ -0,0 +1,225 @@
+const express = require('express')
+const { ArgumentParser } = require('argparse')
+const cloneDeep = require('lodash.clonedeep')
+
+const config = {
+  port: 8080,
+}
+
+//////////////////////////////////
+////////Arguments parser//////////
+//////////////////////////////////
+const parser = new ArgumentParser({
+  description: 'HTTP/TCP/UDP server to generate benchmark for benchmark tools'
+})
+parser.add_argument('-p', '--port', {
+  type: 'int',
+  help: 'Port for HTTP Control API'
+})
+
+const parsed_args = parser.parse_args()
+if (parsed_args.port) {
+  config.port = parsed_args.port
+}
+
+
+//////////////////////////////////
+///////////////State//////////////
+//////////////////////////////////
+const Status = {
+  Started: Symbol('Started'),
+  Stopped: Symbol('Stopped'),
+}
+const Type = {
+  TCP: 'tcp',
+  UDP: 'udp',
+  HTTP: 'http',
+}
+const defaultState = {
+  status: Status.Stopped,
+  server: null,
+  time: {
+    startedAt: null,
+    stoppedAt: null,
+  },
+  benchmarkServer: {
+    type: null,
+    port: null,
+  },
+  stats: {
+    requests: {
+      total: {
+        count: 0,
+        bytes: 0,
+      },
+    }
+  }
+}
+let globalState = cloneDeep(defaultState)
+
+function getStats(state) {
+  const upperBound = state.time.stoppedAt || new Date()
+  const { time: { startedAt }, stats } = state
+  if (startedAt === null) {
+    return null
+  }
+
+  const ms = upperBound.getTime() - startedAt.getTime()
+  const seconds = Math.floor(ms / 1000)
+  return {
+    type: state.benchmarkServer.type,
+    duration: {
+      seconds,
+    },
+    requests: {
+      average: {
+        perSeconds: {
+          count: stats.requests.total.count / seconds,
+          bytes: stats.requests.total.bytes / seconds,
+        },
+      },
+      ...stats.requests,
+    },
+  }
+}
+
+function resetGlobalState() {
+  globalState = cloneDeep(defaultState)
+}
+//////////////////////////////////
+////////Benchmark Server//////////
+//////////////////////////////////
+function startHTTPBenchmarkServer(state, cb = () => {}) {
+  const HTTPBenchmarkAPI = express()
+
+  HTTPBenchmarkAPI.use(function (req, res) {
+    const bytes = req.socket.bytesRead || req.get('content-length') || 0
+    state.stats.requests.total.count += 1
+    state.stats.requests.total.bytes += bytes
+    res.status(200).send()
+  })
+
+  return HTTPBenchmarkAPI
+    .listen(state.benchmarkServer.port, () => cb({
+      code: 200,
+      message: `HTTP Control API is listening on port ${state.benchmarkServer.port}`,
+    }))
+    .on('error', (err) => cb({
+      code: 200,
+      message: `HTTP Benchmark Server stopped with error ${err}`,
+    }))
+}
+
+function startBenchmarkServer(state, cb) {
+  globalState.status = Status.Started
+  globalState.time.startedAt = new Date()
+
+  const { type } = state.benchmarkServer
+  switch (type) {
+    case Type.HTTP: return startHTTPBenchmarkServer(state, cb)
+  }
+  
+  return cb({ code: 500, message: `Undefined type provided: ${type}`})
+}
+
+function stopBenchmarkServer(state, cb = () => {}) {
+  state.status = Status.Stopped
+  globalState.time.stoppedAt = new Date()
+
+  const { type } = state.benchmarkServer
+  switch (type) {
+    case Type.HTTP: {
+      state.server.close()
+      break
+    }
+  }
+
+  // XXX Smelly
+  setTimeout(cb, 1000)
+}
+
+//////////////////////////////////
+////////HTTP Control API//////////
+//////////////////////////////////
+const httpControlAPI = express()
+
+// TODO add validation module, like joi
+httpControlAPI.get('/start/:type/:port/:duration?', (req, res) => {
+  const { type } = req.params
+  const port = Number.parseInt(req.params.port)
+  const duration = Number.parseInt(req.params.duration)
+  if (globalState.status !== Status.Stopped) {
+    return res.status(400).send({
+      code: 400,
+      message: 'Status should be Stopped before Start',
+    })
+  }
+  if (!Object.values(Type).includes(type)) {
+    return res.status(400).send({
+      code: 400,
+      message: `Provided ${type}, but allowed types are: ${Object.values(Type)}`,
+    })
+  }
+  if(Number.isNaN(port) || port < 1) {
+    return res.status(400).send({
+      code: 400,
+      message: `Port is not valid`,
+    })
+  }
+  if (req.params.duration && (Number.isNaN(duration) || duration < 1)) {
+    return res.status(400).send({
+      code: 400,
+      message: `Duration is not valid`,
+    })
+  }
+
+  resetGlobalState()
+  globalState.benchmarkServer.type = type
+  globalState.benchmarkServer.port = port
+
+  try {
+    globalState.server = startBenchmarkServer(globalState, ({ code, message }) => {
+      res.status(code).send({ code, message })
+      if (code !== 200) {
+        resetGlobalState()
+      }
+    })
+
+    if (duration) {
+      setTimeout(() => stopBenchmarkServer(globalState), duration * 1000)
+    }
+  } catch(e) {
+    res.status(500).send({ code: 500, message: e.message })
+    resetGlobalState()
+  }
+})
+
+httpControlAPI.get('/stop', (req, res) => {
+  if (globalState.status !== Status.Started) {
+    res.status(400).send({ code: 400 })
+  }
+
+  try {
+    stopBenchmarkServer(globalState, () => {
+      res.status(200).send(getStats(globalState))
+    })
+  } catch(e) {
+    res.status(500).send({ code: 500, message: e.message })
+  }
+})
+
+httpControlAPI.get('/stats', (req, res) => {
+  try {
+    res.status(200).send(getStats(globalState))
+  } catch (e) {
+    res.status(500).send({ code: 500, message: e.message })
+  }
+})
+
+httpControlAPI.use(function (req, res, next) {
+  res.status(404).send({ code: 404 })
+})
+
+httpControlAPI
+  .listen(config.port, () => console.log(`HTTP Control API is listening on port ${config.port}`))
+  .on('error', (err) => console.log(`HTTP Control API stopped with error ${err}`))
