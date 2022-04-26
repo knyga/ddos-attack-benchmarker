75c331584e0dda0d9869a8fcfd25c430bd97963c
Oleksandr Knyga <oleksandrknyga@gmail.com>
2022-04-26 14:14:25 +0300

    pm2 added

--- app.js
+++
@@ -1,9 +0,0 @@
-const { parser } = require('./src/args-parser')
-const { startControllerServer } = require('./src/controller-server')
-
-const config = parser.parse_args()
-const server = startControllerServer(config)
-
-if(config.duration) {
-  setTimeout(() => server.close(), config.duration * 1000)
-}
--- src/args-parser.js
+++ src/args-parser.js
@@ -5,13 +5,21 @@ const parser = new ArgumentParser({
   description: 'HTTP/TCP/UDP server to generate benchmark for benchmark tools',
 })
 parser.add_argument('-p', '--port', {
+  dest: 'port',
   type: 'int',
   default: 80,
   help: 'Port for HTTP Control API',
 })
-parser.add_argument('-d', '--duration', {
+parser.add_argument('-l', '--duration', {
+  dest: 'duration',
   type: 'int',
   help: 'Lifetime in seconds',
+})
+parser.add_argument('-d', '--detach', {
+  dest: 'detach',
+  action: 'store_true',
+  default: false,
+  help: 'Detach process from execution',
 })
 
 module.exports = {
